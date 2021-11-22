4c10a48b0420dd0f2ab4cdc7bbb7211a2b949c0b
Indiana Chandisingh <ichandisingh@softwareinstitute.com>
2021-11-22 17:46:28 +0000

    change test names

--- src/test/java/code/indiana/chandisingh/minesweepertest/TileTest.java
+++ src/test/java/code/indiana/chandisingh/minesweepertest/TileTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TileTest {
 
     @Test
-    public void checkTileTypeTest(){
+    public void testCheckTileType(){
         Tile tile=new Tile();
         tile.setTileType(true);
         assertEquals(true,tile.checkTileType(),"Incorrect tile type returned.");
